f6149b69d560fe826885d8e0350a6803dfe9a993
cunitac <cunitac@gmail.com>
2021-06-10 16:53:17 +0900

    make solution easier

--- 002.py
+++ 002.py
@@ -4,10 +4,9 @@ for num in reversed(range(0, 1 << N, 2)):
     bits = list(bin(num)[2:].zfill(N))
     if bits.count('1') != bits.count('0'):
         continue
-    i, j = -1, -1
     try:
         for _ in range(N // 2):
-            i = bits.index('1', i + 1)
+            i = bits.index('1')
             j = bits.index('0', i + 1)
             bits[i] = ' '
             bits[j] = ' '
