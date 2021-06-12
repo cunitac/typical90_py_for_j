bfe1b4d02a74aab2a4b5cc363e298dc28189fb59
cunitac <cunitac@gmail.com>
2021-06-12 15:38:13 +0900

    solved 048

---
+++ 048.py
@@ -0,0 +1,11 @@
+N, K = map(int, input().split())
+points = []
+
+for _ in range(N):
+    A, B = map(int, input().split())
+    points.append(B)
+    points.append(A-B)
+
+points.sort(reverse=True)
+
+print(sum(points[:K]))
